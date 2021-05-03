e569a07c87ee3293f70461860b2c8da54fefa1ac
Luis Fernando Ruiz <luisfruizlopez@gmail.com>
2021-05-03 18:47:53 -0500

    Filter table 1

--- static/js/table.js
+++ static/js/table.js
@@ -1,5 +1,3 @@
-// Append the dropdown button for the regions.
-
 function dropdown() {
     regions.forEach(region => {
         let dropdownRegion = d3.select("#region");
@@ -8,140 +6,102 @@ function dropdown() {
     })
 };
 
-dropdown()
+dropdown();
+
+var table = document.querySelector("tbody");
+
+function generateTable(table, data) {
+    console.log('loading...')
+    //d3.event.preventDefault()
+    tableHtml = d3.select("tbody");
+    tableHtml.html("");
+    for (let element of data) {
+        // add a row 
+        let row = table.insertRow();
+        for (key in element) {
+            // creates a new cell
+            let cell = row.insertCell();
+            // creates a new text node
+            let text = document.createTextNode(element[key]);
+            // appends the text node to the cell
+            cell.appendChild(text);
+        }
+    }
+};
 
 d3.json("../static/js/data.json").then(data=> {
-    console.log(data[0])
-    generateTable(data)
-})
+    console.log("promise fullfilled");
+    generateTable(table, data);
 
-
-
-// filter the table
-function filterData(userInput) {
-    let inputValue = {}
-    let date = d3.select("#date").property('value')
-    let price = d3.select("#price").property('value')
-    let volume = d3.select("#volume").property('value')
-    let type = d3.select("#type").property('value')
-    let region = d3.select("#region").property('value')
-    
-    inputValue[date] = date
-    inputValue[price] = price
-    inputValue[volume] = volume
-    inputValue[type] = type
-    inputValue[region] = region
-
-    console.log(inputValue)
-    return inputValue
-}
-
-//console.log(filterData())
-
-// read the data
-
-// d3.json('/table').then (data=> 
-//     console.log(data)
-//     )
-// /Tableau/avocado-updated-2020.csv
-// d3.json("/data").then(data=> {
-//     // console.log(data[0])
-//     // console.log(data[0].date)
-//     data.forEach(element => {
-//         console.log(element)
-
-//     });
-
-// })
-
-// console.log(tableData)
-// console.log(Object.entries)
-// console.log(tableData[0])
-
-// just to know button clicked 
-d3.selectAll("button").on("click", function () {
-    console.log(this)
 });
 
+function filterData(){
+    console.log("filtering...");
+
+    tableHtml = d3.select("tbody");
+    tableHtml.html("");
 
 
-function generateTable(data) {
-    tableBody = d3.select("tbody")
-    // tableHead = d3.select('thead')
-    // console.log(Object.entries(data))
-    // let rowHead = tableHead.append("tr")
-    // let cellHead = rowHead.append("th")
-    // cellHead.text(key)
-    // add a row
-    data.forEach(element =>{
+    let date = d3.select("#date").property('value');
+    let price = d3.select("#price").property('value');
+    let type = d3.select("#type").property('value');
+    let region = d3.select("#region").property('value');
+    price = parseFloat(price);
+
+    console.log(date,price,type,region);
+    
+    d3.json("../static/js/data.json").then(data=> {
+        console.log("promise fullfilled")
         
-        let rowBody = tableBody.append("tr")
-        Object.entries(element).forEach(([key, value]) => {
+        var filterTable = data.filter(item => {(item.date === date)
+            && (item.price >= price)
         
-        let cellBody = rowBody.append("td");
-        cellBody.text(value);
+            && (item.type === type)
+            && (item.geography === region);
+        console.log("filterTable", filterTable);
         
         });
-    })
-};
-// grab table and pass that to our function
-// let table = document.querySelector("tbody");
-// generateTable(table, tableData);
-// --------------------------------------------
-//
+        generateTable(table, filterTable);
 
-// get the input
-
-
-
-//filter the table function
-function filterData(userInput) {
-    let inputValue = {}
-    let date = d3.select("#date").property('value')
-    let price = d3.select("#price").property('value')
-    let volume = d3.select("#volume").property('value')
-    let type = d3.select("#type").property('value')
-    let region = d3.select("#region").property('value')
+    });
     
-    inputValue[date] = date
-    inputValue[price] = price
-    inputValue[volume] = volume
-    inputValue[type] = type
-    inputValue[region] = region
 
-    console.log(inputValue)
-    return inputValue
-}
+    // event.preventDefault();
 
-console.log(filterData())
+    // select the button(event)
+    
 
-// console.log(d3.select("#date"))
 
-// select the button(event)
-filterButton = d3.select("#filter-btn");
-
-// call `on` (event listnener) to run the function that will work 
-filterButton.on("click", () => {
-    console.log(filterData())
+    // call `on` (event listener) to run the function that will work 
+    
+        // console.log(this)
     // filter the input from the table
-    // filterTable = tableData.filter(item => (item.datetime === filterData("#datetime") || filterData("#datetime") === "")
-    //     && (item.city === filterData("#city") || filterData("#city") === "")
-    //     && (item.state === filterData("#state") || filterData("#state") === "")
-    //     && (item.country === filterData("#country") || filterData("#country") === "")
-    //     && (item.shape === filterData("#shape") || filterData("#shape") === ""));
+    //     filterTable = tableData.filter(item => (item.date === filterData("#date") || filterData("#date") === "")
+    //         && (item.price >= filterData("#price") || filterData("#price") === "")
+            
+    //         && (item.type === filterData("#type") || filterData("#type") === "")
+    //         && (item.region === filterData("#region") || filterData("#region") === ""));
     // console.log("filterTable", filterTable);
     // generateTable(table, filterTable);
 
-});
 
-// reset the table from the begnning button 
-resetButton = d3.select("#reset-btn")
-resetButton.on("click", () => {
-    generateTable(table, tableData)
-    // clear the input fileds 
-    document.getElementById('date').value = ''
-    document.getElementById('price').value = ''
-    document.getElementById('volume').value = ''
-    document.getElementById('country').value = ''
-    document.getElementById('shape').value = ''
-});
+};
+
+
+    
+
+    
+
+
+
+// function optionChanged(userChoice) {
+//     filterData(userChoice)
+// }
+
+
+
+filterButton = d3.select("#filter-btn");
+filterButton.on("click", ()=>{
+    console.log('filter button');
+    filterData();
+})
